179f38757bc309b6d7b8b7e8a2259347a59ecf93
Dawid Kostka <dpkostka@gmail.com>
2022-01-07 18:16:57 +0100

    fixed footer + search

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import tmdb from "../../img/tmdb.svg";
 function Footer() {
   return (
     <div className="footer">
-      <h5>Copypright &copy; dkostka.dev</h5>
+      <h5>Copyright &copy; dkostka.dev</h5>
       <img src={tmdb} alt="the movie db logo" className="tmdb" />
     </div>
   );
