5f411426dedc17497fe3d116348c0be8c8e325ea
Dawid Kostka <dpkostka@gmail.com>
2022-08-01 13:24:06 +0200

    minor changes

--- src/components/MuvizGrid/MuvizGrid.jsx
+++ src/components/MuvizGrid/MuvizGrid.jsx
@@ -3,43 +3,6 @@ import MuviItem from "../MuvizItem/MuviItem";
 import "./muvizgrid.css";
 
 function MuvizGrid({ items }) {
-  {
-    /*}
-  const [items, setItems] = useState([]);
-  
-  const [hidden, setHidden] = useState(false);
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      const result = await axios(
-        `https://api.themoviedb.org/3/discover/movie?api_key=52291c7d63b8c123354a244384d96744&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${page}&vote_count.gte=1000&with_watch_monetization_types=flatrate`
-      );
-      //console.log(result.data);
-      //console.log(result.data.results);
-
-      setItems(result.data.results);
-    };
-
-    fetchItems();
-  }, [page]);
-*/
-  }
-  {
-    /*}
-  const hideShowBtn = () => {
-    console.log(page);
-    if (page === 0) {
-      setHidden(true);
-      setPage(page - 1);
-      console.log(page);
-    } else {
-      setHidden(false);
-      setPage(page - 1);
-      console.log(page);
-    }
-  };
-*/
-  }
   return (
     <div className="muviz-grid">
       <section className="cards">
